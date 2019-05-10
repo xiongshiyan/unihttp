f3e7866b28456216312b830748729f36216e0ee2
xxssyyyyssxx <xxssyyyyssxx@126.com>
2019-05-10 09:24:03 +0800

    各个模块在spring-boot下的starter配置

---
+++ httpclient-apache-spring-boot-starter/src/main/java/top/jfunc/common/http/boot/SmartHttpAutoConfigure.java
@@ -0,0 +1,44 @@
+package top.jfunc.common.http.boot;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import top.jfunc.common.http.base.Config;
+import top.jfunc.common.http.smart.ApacheSmartHttpClient;
+import top.jfunc.common.http.smart.SmartHttpClient;
+
+/**
+ * @author xiongshiyan at 2019/5/10 , contact me with email yanshixiong@126.com or phone 15208384257
+ */
+@Configuration
+@EnableConfigurationProperties(SmartHttpProperties.class)
+public class SmartHttpAutoConfigure {
+
+    @Autowired
+    SmartHttpProperties smartHttpProperties;
+
+    @Bean(name = "smartHttpClient")
+    @ConditionalOnMissingBean
+    public SmartHttpClient smartHttpClient(){
+        SmartHttpClient smartHttpClient = new ApacheSmartHttpClient();
+
+        Config config = Config.defaultConfig();
+        if(null != smartHttpProperties.getBaseUrl()){
+            config.setBaseUrl(smartHttpProperties.getBaseUrl());
+        }
+        config.setDefaultConnectionTimeout(smartHttpProperties.getDefaultConnectionTimeout());
+        config.setDefaultReadTimeout(smartHttpProperties.getDefaultReadTimeout());
+        config.setDefaultBodyCharset(smartHttpProperties.getDefaultBodyCharset());
+        config.setDefaultResultCharset(smartHttpProperties.getDefaultResultCharset());
+
+        if(null != smartHttpProperties.getDefaultHeaders()){
+            config.setDefaultHeaders(smartHttpProperties.getDefaultHeaders());
+        }
+
+        smartHttpClient.setConfig(config);
+        
+        return smartHttpClient;
+    }
+}
---
+++ httpclient-jdk-spring-boot-starter/src/main/java/top/jfunc/common/http/boot/SmartHttpProperties.java
@@ -0,0 +1,86 @@
+package top.jfunc.common.http.boot;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import top.jfunc.common.http.HttpConstants;
+
+import java.util.Map;
+
+/**
+ * @see top.jfunc.common.http.base.Config
+ * @author xiongshiyan at 2019/5/10 , contact me with email yanshixiong@126.com or phone 15208384257
+ */
+@ConfigurationProperties("spring.http.smart")
+public class SmartHttpProperties {
+    /**
+     * BaseUrl,如果设置了就在正常传送的URL之前添加上
+     */
+    private String baseUrl                                  = null;
+    /**
+     * 连接超时时间
+     */
+    private Integer defaultConnectionTimeout                = HttpConstants.DEFAULT_CONNECT_TIMEOUT;
+    /**
+     * 读数据超时时间
+     */
+    private Integer defaultReadTimeout                      = HttpConstants.DEFAULT_READ_TIMEOUT;
+    /**
+     * 请求体编码
+     */
+    private String defaultBodyCharset                       = HttpConstants.DEFAULT_CHARSET;
+    /**
+     * 返回体编码
+     */
+    private String defaultResultCharset                     = HttpConstants.DEFAULT_CHARSET;
+    /**
+     * 默认headers
+     */
+    private Map<String , String> defaultHeaders             = null;
+
+    public String getBaseUrl() {
+        return baseUrl;
+    }
+
+    public void setBaseUrl(String baseUrl) {
+        this.baseUrl = baseUrl;
+    }
+
+    public Integer getDefaultConnectionTimeout() {
+        return defaultConnectionTimeout;
+    }
+
+    public void setDefaultConnectionTimeout(Integer defaultConnectionTimeout) {
+        this.defaultConnectionTimeout = defaultConnectionTimeout;
+    }
+
+    public Integer getDefaultReadTimeout() {
+        return defaultReadTimeout;
+    }
+
+    public void setDefaultReadTimeout(Integer defaultReadTimeout) {
+        this.defaultReadTimeout = defaultReadTimeout;
+    }
+
+    public String getDefaultBodyCharset() {
+        return defaultBodyCharset;
+    }
+
+    public void setDefaultBodyCharset(String defaultBodyCharset) {
+        this.defaultBodyCharset = defaultBodyCharset;
+    }
+
+    public String getDefaultResultCharset() {
+        return defaultResultCharset;
+    }
+
+    public void setDefaultResultCharset(String defaultResultCharset) {
+        this.defaultResultCharset = defaultResultCharset;
+    }
+
+    public Map<String, String> getDefaultHeaders() {
+        return defaultHeaders;
+    }
+
+    public void setDefaultHeaders(Map<String, String> defaultHeaders) {
+        this.defaultHeaders = defaultHeaders;
+    }
+}
