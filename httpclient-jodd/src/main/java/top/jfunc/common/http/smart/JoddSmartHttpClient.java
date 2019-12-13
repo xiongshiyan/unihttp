ff3aab8ff7096b392dac1651b9cfb6b3ce9419f0
xxssyyyyssxx <xxssyyyyssxx@126.com>
2019-12-13 13:12:22 +0800

    JoddSmartHttpClient增强扩展性

--- httpclient-jodd/src/main/java/top/jfunc/common/http/smart/JoddSmartHttpClient.java
+++ httpclient-jodd/src/main/java/top/jfunc/common/http/smart/JoddSmartHttpClient.java
@@ -7,6 +7,7 @@ import top.jfunc.common.http.Method;
 import top.jfunc.common.http.base.ContentCallback;
 import top.jfunc.common.http.base.FormFile;
 import top.jfunc.common.http.base.ResultCallback;
+import top.jfunc.common.http.util.JoddUtil;
 import top.jfunc.common.utils.MultiValueMap;
 
 import java.io.IOException;
@@ -30,22 +31,7 @@ public class JoddSmartHttpClient extends AbstractSmartHttpClient<HttpRequest> {
             /// RouteParamHolder routeParamHolder = httpRequest.routeParamHolder();
             /// String completedUrl = handleUrlIfNecessary(httpRequest.getUrl() , routeParamHolder.getMap() , queryParamHolder.getParams() , queryParamHolder.getParamCharset());
             String completedUrl = handleUrlIfNecessary(httpRequest.getUrl());
-
-            HttpRequest request = new HttpRequest();
-            request.method(method.name());
-            request.set(completedUrl);
-
-
-            //2.超时设置
-            request.connectionTimeout(getConnectionTimeoutWithDefault(httpRequest.getConnectionTimeout()));
-            request.timeout(getReadTimeoutWithDefault(httpRequest.getReadTimeout()));
-
-            //3.SSL设置
-            initSSL(request , getHostnameVerifierWithDefault(httpRequest.getHostnameVerifier()) ,
-                    getSSLSocketFactoryWithDefault(httpRequest.getSslSocketFactory()) ,
-                    getX509TrustManagerWithDefault(httpRequest.getX509TrustManager()),
-                    getProxyInfoWithDefault(httpRequest.getProxyInfo()));
-
+            HttpRequest request = createAndConfigHttpRequest(httpRequest, method, completedUrl);
 
             //4.处理body
             if(contentCallback != null && method.hasContent()){
@@ -53,11 +39,7 @@ public class JoddSmartHttpClient extends AbstractSmartHttpClient<HttpRequest> {
             }
 
             //5.设置header
-            MultiValueMap<String, String> headers = mergeDefaultHeaders(httpRequest.getHeaders());
-
-            headers = handleCookieIfNecessary(completedUrl, headers);
-
-            setRequestHeaders(request , httpRequest.getContentType() , headers);
+            configRequestHeaders(request , httpRequest , completedUrl);
 
             //6.子类可以复写
             doWithHttpRequest(request , httpRequest);
@@ -66,24 +48,12 @@ public class JoddSmartHttpClient extends AbstractSmartHttpClient<HttpRequest> {
             response = request.send();
 
             //8.返回header,包括Cookie处理
-            boolean includeHeaders = httpRequest.isIncludeHeaders();
-            if(supportCookie()){
-                includeHeaders = top.jfunc.common.http.request.HttpRequest.INCLUDE_HEADERS;
-            }
-            MultiValueMap<String, String> parseHeaders = parseHeaders(response, includeHeaders);
-
-            //存入Cookie
-            if(supportCookie()){
-                if(null != getCookieHandler() && null != parseHeaders){
-                    CookieHandler cookieHandler = getCookieHandler();
-                    cookieHandler.put(URI.create(completedUrl) , parseHeaders);
-                }
-            }
+            MultiValueMap<String , String> responseHeaders = parseResponseHeaders(response , httpRequest , completedUrl);
 
             return resultCallback.convert(response.statusCode(),
                     getStreamFrom(response, httpRequest.isIgnoreResponseBody()),
                     getResultCharsetWithDefault(httpRequest.getResultCharset()),
-                    parseHeaders);
+                    responseHeaders);
         } finally {
             if(null != response){
                 response.close();
@@ -91,12 +61,62 @@ public class JoddSmartHttpClient extends AbstractSmartHttpClient<HttpRequest> {
         }
     }
 
+    protected HttpRequest createAndConfigHttpRequest(top.jfunc.common.http.request.HttpRequest httpRequest , Method method , String completedUrl){
+        HttpRequest request = new HttpRequest();
+        request.method(method.name());
+        request.set(completedUrl);
+
+
+        //2.超时设置
+        request.connectionTimeout(getConnectionTimeoutWithDefault(httpRequest.getConnectionTimeout()));
+        request.timeout(getReadTimeoutWithDefault(httpRequest.getReadTimeout()));
+
+        //3.SSL设置
+        initSSL(request , httpRequest);
+
+        return request;
+    }
+
+    protected void initSSL(HttpRequest request , top.jfunc.common.http.request.HttpRequest httpRequest){
+        JoddUtil.initSSL(request , getHostnameVerifierWithDefault(httpRequest.getHostnameVerifier()) ,
+                getSSLSocketFactoryWithDefault(httpRequest.getSslSocketFactory()) ,
+                getX509TrustManagerWithDefault(httpRequest.getX509TrustManager()),
+                getProxyInfoWithDefault(httpRequest.getProxyInfo()));
+    }
+
+    protected void configRequestHeaders(HttpRequest request , top.jfunc.common.http.request.HttpRequest httpRequest , String completedUrl) throws IOException{
+        MultiValueMap<String, String> headers = mergeDefaultHeaders(httpRequest.getHeaders());
+
+        headers = handleCookieIfNecessary(completedUrl, headers);
+
+        setRequestHeaders(request , httpRequest.getContentType() , headers);
+    }
     /**
      * 留给子类做更多的配置
      * @param joddHttpRequest jodd的请求
      * @param httpRequest 请求参数
      */
     protected void doWithHttpRequest(HttpRequest joddHttpRequest , top.jfunc.common.http.request.HttpRequest httpRequest){}
+
+
+    protected MultiValueMap<String , String> parseResponseHeaders(HttpResponse response , top.jfunc.common.http.request.HttpRequest httpRequest , String completedUrl) throws IOException{
+        boolean includeHeaders = httpRequest.isIncludeHeaders();
+        if(supportCookie()){
+            includeHeaders = top.jfunc.common.http.request.HttpRequest.INCLUDE_HEADERS;
+        }
+        MultiValueMap<String, String> parseHeaders = parseHeaders(response, includeHeaders);
+
+        //存入Cookie
+        if(supportCookie()){
+            if(null != getCookieHandler() && null != parseHeaders){
+                CookieHandler cookieHandler = getCookieHandler();
+                cookieHandler.put(URI.create(completedUrl) , parseHeaders);
+            }
+        }
+
+        return parseHeaders;
+    }
+
 
     @Override
     protected ContentCallback<HttpRequest> bodyContentCallback(Method method , String body, String bodyCharset, String contentType) throws IOException {
