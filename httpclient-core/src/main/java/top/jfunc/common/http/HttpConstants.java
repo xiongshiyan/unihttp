3360bbf1390aedc0d5083f97966cb6dc11b46c56
xxssyyyyssxx <xxssyyyyssxx@126.com>
2019-12-16 18:29:55 +0800

    HttpConstants去除对系统变量改变默认值的支持，不方便使用

--- httpclient-core/src/main/java/top/jfunc/common/http/HttpConstants.java
+++ httpclient-core/src/main/java/top/jfunc/common/http/HttpConstants.java
@@ -1,6 +1,5 @@
 package top.jfunc.common.http;
 
-import top.jfunc.common.propenv.EnvUtil;
 import top.jfunc.common.utils.CharsetUtil;
 
 /**
@@ -28,28 +27,12 @@ public class HttpConstants {
 
 
 
-    public static final Integer DEFAULT_CONNECT_TIMEOUT;
-    public static final Integer DEFAULT_READ_TIMEOUT;
-    public static final String  DEFAULT_CHARSET;
+    public static final Integer DEFAULT_CONNECT_TIMEOUT  = 15000;
+    public static final Integer DEFAULT_READ_TIMEOUT     = 15000;
+    public static final String  DEFAULT_CHARSET          = CharsetUtil.UTF_8;
 
     public static final String FORM_URLENCODED                      = "application/x-www-form-urlencoded";
     public static final String JSON                                 = "application/json";
-    public static final String TEXT_XML                             = "text/xml";
-    public static final String FORM_URLENCODED_WITH_DEFAULT_CHARSET;
-    public static final String TEXT_XML_WITH_DEFAULT_CHARSET       ;
-    public static final String JSON_WITH_DEFAULT_CHARSET           ;
-
-    static {
-        //给应用一个修改默认值的机会,使用System.setProperty()或者-D来设置
-        String dct = EnvUtil.env("DEFAULT_CONNECT_TIMEOUT");
-        String drt = EnvUtil.env("DEFAULT_READ_TIMEOUT");
-        String dc = EnvUtil.env("DEFAULT_CHARSET");
-        DEFAULT_CONNECT_TIMEOUT = (dct==null) ? 15000 : Integer.valueOf(dct);
-        DEFAULT_READ_TIMEOUT    = (drt==null) ? 15000 : Integer.valueOf(drt);
-        DEFAULT_CHARSET = ( dc == null ) ? CharsetUtil.UTF_8 : dc;
-
-        FORM_URLENCODED_WITH_DEFAULT_CHARSET = FORM_URLENCODED + ";charset=" + DEFAULT_CHARSET;
-        TEXT_XML_WITH_DEFAULT_CHARSET        = TEXT_XML + ";charset=" + DEFAULT_CHARSET;
-        JSON_WITH_DEFAULT_CHARSET            = JSON + ";charset=" + DEFAULT_CHARSET;
-    }
+    public static final String FORM_URLENCODED_WITH_DEFAULT_CHARSET = MediaType.APPLICATIPON_FORM_DATA.withCharset(DEFAULT_CHARSET).toString();
+    public static final String JSON_WITH_DEFAULT_CHARSET            = MediaType.APPLICATIPON_JSON.withCharset(DEFAULT_CHARSET).toString();
 }
