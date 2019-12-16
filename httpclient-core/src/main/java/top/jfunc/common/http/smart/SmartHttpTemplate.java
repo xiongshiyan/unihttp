163312d58204a04bc12e710fe56de6c17f00126d
xxssyyyyssxx <xxssyyyyssxx@126.com>
2019-12-16 10:36:12 +0800

    SmartHttpTemplate没必要继承HttpTemplate

--- httpclient-core/src/main/java/top/jfunc/common/http/smart/SmartHttpTemplate.java
+++ httpclient-core/src/main/java/top/jfunc/common/http/smart/SmartHttpTemplate.java
@@ -3,7 +3,6 @@ package top.jfunc.common.http.smart;
 import top.jfunc.common.http.Method;
 import top.jfunc.common.http.base.ContentCallback;
 import top.jfunc.common.http.base.ResultCallback;
-import top.jfunc.common.http.basic.HttpTemplate;
 import top.jfunc.common.http.request.HttpRequest;
 
 import java.io.IOException;
@@ -11,7 +10,7 @@ import java.io.IOException;
 /**
  * @author xiongshiyan at 2019/4/2 , contact me with email yanshixiong@126.com or phone 15208384257
  */
-public interface SmartHttpTemplate<C> extends HttpTemplate<C> {
+public interface SmartHttpTemplate<C> {
     /**
      * 使用Request来放请求数据
      * @param httpRequest 请求参数
