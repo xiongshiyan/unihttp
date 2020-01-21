583dc91b9419e76edaa83234e5b6e008df44f01e
xxssyyyyssxx <xxssyyyyssxx@126.com>
2020-01-21 09:44:44 +0800

    CloseNoneCloser

--- httpclient-apache/src/main/java/top/jfunc/common/http/component/apache/SingleApacheClientFactory.java
+++ httpclient-apache/src/main/java/top/jfunc/common/http/component/apache/SingleApacheClientFactory.java
@@ -4,12 +4,13 @@ import org.apache.http.client.HttpClient;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import top.jfunc.common.http.component.AbstractRequesterFactory;
+import top.jfunc.common.http.component.CloseNoneCloser;
 import top.jfunc.common.http.request.HttpRequest;
 
 import java.io.IOException;
 
 /**
- * 要配合{@link top.jfunc.common.http.component.NoneCloser}不能关闭这个client
+ * 要配合{@link CloseNoneCloser}不能关闭这个client
  * @see DefaultApacheClientFactory
  * @author xiongshiyan at 2020/1/6 , contact me with email yanshixiong@126.com or phone 15208384257
  */
--- httpclient-core/src/main/java/top/jfunc/common/http/component/CloseNoneCloser.java
+++ httpclient-core/src/main/java/top/jfunc/common/http/component/CloseNoneCloser.java
@@ -6,7 +6,7 @@ import java.io.IOException;
 /**
  * @author xiongshiyan at 2020/1/21 , contact me with email yanshixiong@126.com or phone 15208384257
  */
-public class NoneCloser implements Closer {
+public class CloseNoneCloser implements Closer {
     @Override
     public void close(Closeable closeable) throws IOException {
         // DO NOTHING TO CLOSE NONE
