add0b0ee42330a21a72319c492f506321be1959d
xxssyyyyssxx <xxssyyyyssxx@126.com>
2020-01-21 09:42:17 +0800

    SingleApacheClientFactory

---
+++ httpclient-apache/src/main/java/top/jfunc/common/http/component/apache/SingleApacheClientFactory.java
@@ -0,0 +1,23 @@
+package top.jfunc.common.http.component.apache;
+
+import org.apache.http.client.HttpClient;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import top.jfunc.common.http.component.AbstractRequesterFactory;
+import top.jfunc.common.http.request.HttpRequest;
+
+import java.io.IOException;
+
+/**
+ * 要配合{@link top.jfunc.common.http.component.NoneCloser}不能关闭这个client
+ * @see DefaultApacheClientFactory
+ * @author xiongshiyan at 2020/1/6 , contact me with email yanshixiong@126.com or phone 15208384257
+ */
+public class SingleApacheClientFactory extends AbstractRequesterFactory<HttpClient> {
+    private static final CloseableHttpClient CLOSEABLE_HTTP_CLIENT = HttpClients.createDefault();
+
+    @Override
+    public HttpClient doCreate(HttpRequest httpRequest) throws IOException {
+        return CLOSEABLE_HTTP_CLIENT;
+    }
+}
---
+++ httpclient-core/src/main/java/top/jfunc/common/http/component/NoneCloser.java
@@ -0,0 +1,14 @@
+package top.jfunc.common.http.component;
+
+import java.io.Closeable;
+import java.io.IOException;
+
+/**
+ * @author xiongshiyan at 2020/1/21 , contact me with email yanshixiong@126.com or phone 15208384257
+ */
+public class NoneCloser implements Closer {
+    @Override
+    public void close(Closeable closeable) throws IOException {
+        // DO NOTHING TO CLOSE NONE
+    }
+}
