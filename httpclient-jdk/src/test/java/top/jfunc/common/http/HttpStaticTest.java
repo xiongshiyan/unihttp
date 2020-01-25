c2639aa10a6eeaad713125fea3122611a1da5eb5
xxssyyyyssxx <xxssyyyyssxx@126.com>
2020-01-25 11:53:42 +0800

    HttpUtilTest->HttpStaticTest

--- httpclient-jdk/src/test/java/top/jfunc/common/http/HttpStaticTest.java
+++ httpclient-jdk/src/test/java/top/jfunc/common/http/HttpStaticTest.java
@@ -8,7 +8,7 @@ import top.jfunc.common.http.smart.SmartHttpClient;
 /**
  * @author xiongshiyan at 2019/5/16 , contact me with email yanshixiong@126.com or phone 15208384257
  */
-public class HttpUtilTest {
+public class HttpStaticTest {
     @Test
     public void testLoad(){
         SmartHttpClient smartHttpClient = HttpStatic.getSmartHttpClient();
--- httpclient-okhttp3/src/test/java/top/jfunc/common/http/HttpStaticTest.java
+++ httpclient-okhttp3/src/test/java/top/jfunc/common/http/HttpStaticTest.java
@@ -2,19 +2,19 @@ package top.jfunc.common.http;
 
 import org.junit.Assert;
 import org.junit.Test;
-import top.jfunc.common.http.smart.ApacheSmartHttpClient;
+import top.jfunc.common.http.smart.OkHttp3SmartHttpClient;
 import top.jfunc.common.http.smart.SmartHttpClient;
 
 /**
  * @author xiongshiyan at 2019/5/16 , contact me with email yanshixiong@126.com or phone 15208384257
  */
-public class HttpUtilTest {
+public class HttpStaticTest {
     @Test
     public void testLoad(){
         SmartHttpClient smartHttpClient = HttpStatic.getSmartHttpClient();
-        Assert.assertTrue(smartHttpClient instanceof ApacheSmartHttpClient);
+        Assert.assertTrue(smartHttpClient instanceof OkHttp3SmartHttpClient);
 
         SmartHttpClient httpClient = HttpDelegate.delegate();
-        Assert.assertTrue(httpClient instanceof ApacheSmartHttpClient);
+        Assert.assertTrue(httpClient instanceof OkHttp3SmartHttpClient);
     }
 }
