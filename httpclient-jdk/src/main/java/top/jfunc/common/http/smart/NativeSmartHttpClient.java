1bc1d82f3d9923eb3da407a537474205013cf9da
xxssyyyyssxx <xxssyyyyssxx@126.com>
2020-01-28 12:07:21 +0800

    创建HttpRequest的方法优化

--- httpclient-jdk/src/main/java/top/jfunc/common/http/smart/NativeSmartHttpClient.java
+++ httpclient-jdk/src/main/java/top/jfunc/common/http/smart/NativeSmartHttpClient.java
@@ -4,7 +4,6 @@ import top.jfunc.common.http.base.*;
 import top.jfunc.common.http.component.*;
 import top.jfunc.common.http.component.jdk.*;
 import top.jfunc.common.http.request.HttpRequest;
-import top.jfunc.common.http.request.basic.CommonRequest;
 import top.jfunc.common.http.util.NativeUtil;
 import top.jfunc.common.utils.MapUtil;
 import top.jfunc.common.utils.MultiValueMap;
@@ -74,17 +73,15 @@ public class NativeSmartHttpClient extends AbstractImplementSmartHttpClient<Http
             int statusCode = connection.getResponseCode();
             if(needRedirect(httpRequest, statusCode, responseHeaders)){
                 String redirectUrl = responseHeaders.getFirst(HttpHeaders.LOCATION);
-                CommonRequest getRequest = CommonRequest.of(redirectUrl);
-                init(getRequest , Method.GET);
-                //处理多次重定向的情况
-                getRequest.followRedirects(HttpRequest.FOLLOW_REDIRECTS);
-                return doInternalTemplate(getRequest, null, resultCallback);
+                HttpRequest hr = createRedirectHttpRequest(httpRequest, redirectUrl);
+                return doInternalTemplate(hr, null, resultCallback);
             }
 
 
             return resultCallback.convert(statusCode, inputStream,
                     getConfig().getResultCharsetWithDefault(httpRequest.getResultCharset()),
                     responseHeaders);
+
         } finally {
             //关闭顺序不能改变，否则服务端可能出现这个异常  严重: java.io.IOException: 远程主机强迫关闭了一个现有的连接
             //1 . 关闭连接
@@ -92,6 +89,14 @@ public class NativeSmartHttpClient extends AbstractImplementSmartHttpClient<Http
             //2 . 关闭流
             closeInputStream(inputStream);
         }
+    }
+
+    protected HttpRequest createRedirectHttpRequest(HttpRequest httpRequest, String redirectUrl) {
+        HttpRequest hr = getHttpRequestFactory().create(redirectUrl , null , null , httpRequest.getConnectionTimeout() , httpRequest.getReadTimeout() , httpRequest.getResultCharset());
+        init(hr , Method.GET);
+        //处理多次重定向的情况
+        hr.followRedirects(HttpRequest.FOLLOW_REDIRECTS);
+        return hr;
     }
 
     /**
