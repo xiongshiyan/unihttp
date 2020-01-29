f25e1da249d2977fb0d1d053b7efd10e4a260de0
xxssyyyyssxx <xxssyyyyssxx@126.com>
2020-01-29 20:52:27 +0800

    HttpDelegate代码优化，用反射去掉大量的if

--- httpclient-core/src/main/java/top/jfunc/common/http/HttpDelegate.java
+++ httpclient-core/src/main/java/top/jfunc/common/http/HttpDelegate.java
@@ -6,52 +6,43 @@ import top.jfunc.common.http.smart.SmartHttpClient;
 import top.jfunc.common.utils.ClassUtil;
 
 /**
- * 动态获取SmartHttpClient的实现类，基于jar包检测 <pre>HttpStatic.delegate()....</pre>
+ * 动态获取SmartHttpClient的实现类，基于jar包检测 <pre>HttpDelegate.delegate()....</pre>
+ * @see HttpStatic
  * @author xiongshiyan at 2017/12/11
  */
 public class HttpDelegate {
     private static final Logger logger = LoggerFactory.getLogger(HttpDelegate.class);
+
     private HttpDelegate(){}
 
+    /**根据类路径的jar加载默认顺序是 OKHttp3、ApacheHttpClient、Jodd、URLConnection*/
+    private static final String[] SMART_HTTP_CLIENT_CLASSES = {"top.jfunc.common.http.smart.OkHttp3SmartHttpClient","top.jfunc.common.http.smart.ApacheSmartHttpClient","top.jfunc.common.http.smart.JoddSmartHttpClient","top.jfunc.common.http.smart.NativeSmartHttpClient"};
     /**
      * http请求工具代理对象
      */
-    private static final SmartHttpClient DELEGATE = initDelegate();
+    private static final SmartHttpClient DELEGATE;
+    static {
+        DELEGATE = initDelegate();
+    }
+
     public static SmartHttpClient delegate() {
         return DELEGATE;
     }
 
+
     private static SmartHttpClient initDelegate() {
+        Class delegateClassToUse = null;
         try {
-            //根据类路径的jar加载默认顺序是 OKHttp3、ApacheHttpClient、JoddHttp、URLConnection
-            Class delegateClassToUse = null;
-            // okhttp3.OkHttpClient ?
-            if (ClassUtil.isPresent(HttpDelegate.class.getClassLoader() ,
-                    "okhttp3.OkHttpClient" , "okio.Okio" ,
-                    "top.jfunc.common.http.smart.OkHttp3SmartHttpClient")) {
-                delegateClassToUse = ClassUtil.loadClass("top.jfunc.common.http.smart.OkHttp3SmartHttpClient");
+            for (String smartHttpClientClass : SMART_HTTP_CLIENT_CLASSES) {
+                if(ClassUtil.isPresent(HttpDelegate.class.getClassLoader() , smartHttpClientClass)){
+                    delegateClassToUse = ClassUtil.loadClass(smartHttpClientClass);
+                    break;
+                }
             }
-            // org.apache.http.impl.client.CloseableHttpClient ?
-            else if (ClassUtil.isPresent(HttpDelegate.class.getClassLoader() ,
-                    "org.apache.http.impl.client.CloseableHttpClient",
-                    "org.apache.http.impl.client.HttpClientBuilder" ,
-                    "top.jfunc.common.http.smart.ApacheSmartHttpClient")) {
-                delegateClassToUse = ClassUtil.loadClass("top.jfunc.common.http.smart.ApacheSmartHttpClient");
-            }
-            // jodd.http.HttpRequest ?
-            else if (ClassUtil.isPresent(HttpDelegate.class.getClassLoader() ,
-                    "jodd.http.HttpRequest","jodd.http.HttpResponse" ,
-                    "top.jfunc.common.http.smart.JoddSmartHttpClient")) {
-                delegateClassToUse = ClassUtil.loadClass("top.jfunc.common.http.smart.JoddSmartHttpClient");
-            }
-            // java.net.URLConnection
-            else if (ClassUtil.isPresent(HttpDelegate.class.getClassLoader() ,
-                    "top.jfunc.common.http.smart.NativeSmartHttpClient")) {
-                delegateClassToUse = ClassUtil.loadClass("top.jfunc.common.http.smart.NativeSmartHttpClient");
-            }
-            return  null != delegateClassToUse ? (SmartHttpClient) delegateClassToUse.newInstance() : null;
-        } catch (Exception e) {
-            logger.warn("自动探测SmartHttpClient失败,请不要使用HttpDelegate,或者引入一种实现");
+
+            return  null == delegateClassToUse ? null : (SmartHttpClient) delegateClassToUse.newInstance();
+        }catch (Exception e){
+            logger.warn("自动探测SmartHttpClient失败,请不要使用HttpDelegate,或者引入一种实现" , e);
             return null;
         }
     }
