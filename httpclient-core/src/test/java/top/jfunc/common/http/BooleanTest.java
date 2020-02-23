2f2056379ab606d7df8f47ee9a4d4523133919e9
xxssyyyyssxx <xxssyyyyssxx@126.com>
2020-02-23 20:07:16 +0800

    BooleanTest#testBoolean

--- httpclient-core/src/test/java/top/jfunc/common/http/BooleanTest.java
+++ httpclient-core/src/test/java/top/jfunc/common/http/BooleanTest.java
@@ -4,6 +4,7 @@ import org.junit.Assert;
 import org.junit.Test;
 import top.jfunc.common.http.base.Config;
 import top.jfunc.common.http.request.basic.CommonRequest;
+import top.jfunc.common.utils.ObjectUtil;
 
 public class BooleanTest {
     @Test
@@ -11,15 +12,15 @@ public class BooleanTest {
         CommonRequest request = CommonRequest.of();
         Config config = Config.defaultConfig();
         //Request未指定的时候使用Config的
-        Assert.assertFalse(config.retainResponseHeadersWithDefault(request.retainResponseHeaders()));
+        Assert.assertFalse(ObjectUtil.defaultIfNull(request.retainResponseHeaders() , config.retainResponseHeaders()));
 
         //request指定了使用Request的
         request.retainResponseHeaders(Config.RETAIN_RESPONSE_HEADERS);
-        Assert.assertTrue(config.retainResponseHeadersWithDefault(request.retainResponseHeaders()));
+        Assert.assertTrue(ObjectUtil.defaultIfNull(request.retainResponseHeaders() , config.retainResponseHeaders()));
 
         //Request未指定的时候使用Config的，Config改变过的
         config.retainResponseHeaders(Config.RETAIN_RESPONSE_HEADERS);
         CommonRequest commonRequest = CommonRequest.of();
-        Assert.assertTrue(config.retainResponseHeadersWithDefault(commonRequest.retainResponseHeaders()));
+        Assert.assertTrue(ObjectUtil.defaultIfNull(commonRequest.retainResponseHeaders() , config.retainResponseHeaders()));
     }
 }
