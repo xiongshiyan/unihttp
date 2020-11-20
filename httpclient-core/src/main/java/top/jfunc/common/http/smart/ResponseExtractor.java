6994f72f30ad0a954bfb3ff1ea8d937de133b938
xxssyyyyssxx <xxssyyyyssxx@126.com>
2020-11-20 17:55:25 +0800

    ResponseExtractor方法公开

--- httpclient-core/src/main/java/top/jfunc/common/http/smart/ResponseExtractor.java
+++ httpclient-core/src/main/java/top/jfunc/common/http/smart/ResponseExtractor.java
@@ -11,43 +11,43 @@ import java.io.InputStream;
  * 根据响应的四要素获取想要的
  * @author xiongshiyan at 2020/1/24 , contact me with email yanshixiong@126.com or phone 15208384257
  */
-class ResponseExtractor {
+public class ResponseExtractor {
     /**
      * 获取响应码
      */
-    static int extractStatusCode(int statusCode , InputStream inputStream , String resultCharset , MultiValueMap<String , String> headers) throws IOException {
+    public static int extractStatusCode(int statusCode , InputStream inputStream , String resultCharset , MultiValueMap<String , String> headers) throws IOException {
         return statusCode;
+    }
+
+    /**
+     * 获取响应header
+     */
+    public static MultiValueMap<String, String> extractHeaders(int statusCode , InputStream inputStream , String resultCharset , MultiValueMap<String , String> headers) throws IOException{
+        return headers;
     }
 
     /**
      * 获取响应体，String
      */
-    static String extractString(int statusCode , InputStream inputStream , String resultCharset , MultiValueMap<String , String> headers) throws IOException{
+    public static String extractString(int statusCode , InputStream inputStream , String resultCharset , MultiValueMap<String , String> headers) throws IOException{
         return null != inputStream ? IoUtil.read(inputStream , resultCharset) : "";
     }
 
     /**
      * 获取响应体，byte[]
      */
-    static byte[] extractBytes(int statusCode , InputStream inputStream , String resultCharset , MultiValueMap<String , String> headers) throws IOException{
+    public static byte[] extractBytes(int statusCode , InputStream inputStream , String resultCharset , MultiValueMap<String , String> headers) throws IOException{
         return null != inputStream ? IoUtil.stream2Bytes(inputStream) : new byte[0];
     }
 
-    /**
-     * 获取响应header
-     */
-    static MultiValueMap<String, String> extractHeaders(int statusCode , InputStream inputStream , String resultCharset , MultiValueMap<String , String> headers) throws IOException{
-        return headers;
-    }
 
 
-
-    static Response toResponse(int statusCode, byte[] bodyBytes, String resultCharset, MultiValueMap<String, String> headers) {
-        return new DefaultResponse(statusCode, bodyBytes, resultCharset, headers);
-    }
-
-    static Response toResponse(int statusCode , InputStream inputStream , String resultCharset , MultiValueMap<String , String> headers) throws IOException{
-        byte[] bodyBytes = null != inputStream ? IoUtil.stream2Bytes(inputStream) : new byte[]{};
+    public static Response toResponse(int statusCode , InputStream inputStream , String resultCharset , MultiValueMap<String , String> headers) throws IOException{
+        byte[] bodyBytes = extractBytes(statusCode, inputStream, resultCharset, headers);
         return toResponse(statusCode, bodyBytes, resultCharset, headers);
+    }
+
+    public static Response toResponse(int statusCode, byte[] bodyBytes, String resultCharset, MultiValueMap<String, String> headers) {
+        return new DefaultResponse(statusCode, bodyBytes, resultCharset, headers);
     }
 }
