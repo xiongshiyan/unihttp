013baa4986c8867afa9010500e996000f412fc53
xxssyyyyssxx <xxssyyyyssxx@126.com>
2020-12-02 14:42:11 +0800

    指示性的注释

--- httpclient-core/src/main/java/top/jfunc/common/http/response/ClientHttpResponseConverter.java
+++ httpclient-core/src/main/java/top/jfunc/common/http/response/ClientHttpResponseConverter.java
@@ -3,7 +3,9 @@ package top.jfunc.common.http.response;
 import java.io.IOException;
 
 /**
- * 结果封装，更通用的封装方式，输入更通用，自己转化成需要的数据结构string、bytes、file...，必须立即使用，因为框架会自动关闭{@link ClientHttpResponse}
+ * 结果封装，更通用的封装方式，输入更通用，自己转化成需要的数据结构string、bytes、file...
+ * 必须立即使用，因为框架会自动关闭{@link ClientHttpResponse}
+ * 常见的几个要素用{@link top.jfunc.common.http.util.ResponseExtractor}即可获取
  * @author xiongshiyan at 2020/11/30 , contact me with email yanshixiong@126.com or phone 15208384257
  */
 @FunctionalInterface
